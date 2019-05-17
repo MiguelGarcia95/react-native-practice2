3bf5de76a95fcd973de423bdc903869ea3b8d52a
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-17 15:48:10 -0500

    changed app page

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,13 +18,9 @@ const instructions = Platform.select({
 
 class App extends Component {
   render() {
-    const imageInfo = {
-      uri: 'https://cs11.pikabu.ru/post_img/big/2019/03/30/1/1553896904296816818.jpg'
-    }
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!!</Text>
-        <Image style={{width: 50, height: 150}} source={imageInfo} />
       </View>
     );
   }
